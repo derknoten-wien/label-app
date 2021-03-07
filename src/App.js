3375b0762eb0497f9077351d11421305201e1452
puschmie <71654862+puschmie@users.noreply.github.com>
2021-03-07 14:02:43 +0100

    MVP Version initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import PdfView from './Pdf-View';
 
-function App() {
+
+
+export default function App() {
+  const [product, setProduct] = useState('Produkt');
+  const [subtitle, setSubtitle] = useState('Untertitel');
+  const [ammount, setAmmount] = useState('0')
+  const [submit, setSubmit] = useState(false);
+
+
+  const handleProductChange = (event)=>{
+    if(submit === false){
+      setProduct(event.target.value);
+    }else{
+      setSubmit(false);
+      setProduct(event.target.value);
+    }
+    
+  }
+  const handleSubtitleChange = (event)=>{
+    if(submit === false){
+      setSubtitle(event.target.value);
+    }else{
+      setSubmit(false);
+      setSubtitle(event.target.value);
+  }
+}
+  const handleAmmountChange = (event)=>{
+    if(submit === false){
+      setAmmount(event.target.value);
+    }else{
+      setSubmit(false);
+      setAmmount(event.target.value);
+  }
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    setSubmit(true);
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+
+        <textarea value={product} onChange={handleProductChange}></textarea>
+        <textarea value={subtitle} onChange={handleSubtitleChange}></textarea>
+        <label>Anzahl:</label>
+        <input type="text" pattern="[0-9]*"value={ammount} onChange={handleAmmountChange}></input>
+        <input type="submit" value="Drucken!"></input>
+      </form>
+      <PdfView submit={submit} product={product} subtitle={subtitle} ammount={ammount}></PdfView>
     </div>
   );
 }
-
-export default App;
---
+++ src/Pdf-View.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import { Page, Text, View, Image, Font, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+import logo from './img/logo.png';
+import bradley from "./img/bradhitc_bold.ttf";
+
+export default function PdfView(props) {
+    Font.register({family: "Bradley", src: bradley})
+
+    let subFontSize = null;
+    const numSWords = props.subtitle.split(' ').length;
+    if((numSWords >= 1)&&(numSWords <= 3)){
+      subFontSize = 12;
+    }else if((numSWords>=4)&&(numSWords<=6)){
+      subFontSize = 8;
+    }else if(numSWords>6){
+      subFontSize = 6;
+    }
+
+    let prodFontSize = null;
+    const numPWords = props.product.split(' ').length;
+    const numChar = props.product.split("").length;
+    if((numPWords === 1)&&(numChar <= 8)){
+      prodFontSize = 18;
+    }else if((numPWords>1)||(numChar<=8)){
+      prodFontSize = 12;
+    }else if(numChar>12){
+      prodFontSize = 10;
+    }else if (numChar>18){
+      prodFontSize = 8;
+    }
+
+    let rows = [];
+    let nRows = Math.ceil(props.ammount / 4);
+    for(let i=0;i<nRows;i++){
+      rows.push(1);
+    }
+
+    const columns = [1,1,1,1];
+
+    const styles = StyleSheet.create({
+        page:{
+          maxHeight: "100%",
+          maxWidth: "100%",
+          display:"flex",
+          justifyContent: "flex-start",
+          alignContent: "center",
+          fontFamily: "Bradley",
+          marginLeft: "5px"
+        },
+        table: {
+          display: "table",
+          maxWidth: "100%"
+        },
+        tableRow:{
+          marginTop: "10px",
+          flexDirection: "row"
+        },
+        tableCol:{
+          width: "25%"
+        },
+        tableCell:{
+          display: "table",
+          flexDirection: "row",
+          justifyContent: "flex-start"
+          //margin: "10px"
+        },
+        logo:{
+          maxWidth: "34%",
+          height: "auto",
+          margin: "0",
+          objectFit: "scale-down"
+        },
+        text:{
+          display:"flex",
+          justifyContent: "flex-start",
+          textAlign: "center",
+          maxWidth: "60%",
+          position: "relative"
+        },
+        ouvertura:{
+          fontSize: "18"
+        },
+        product:{
+          fontSize: prodFontSize
+        },
+        subtitle:{
+          maxWidth: "100%",
+          fontSize: subFontSize
+        }
+
+      });
+      
+    
+      const Labeldoc = () =>{
+        return(
+          <Document>
+            <Page size="A4" orientation="landscape" style={styles.page}>
+              <View style={styles.table}>
+                {rows.map(()=>{
+                  return(
+                    <View wrap={false} style={styles.tableRow}>
+                      {columns.map(()=>{
+                        return(
+                          <View style={styles.tableCol}>
+                            <View style={styles.tableCell}>
+                              <Image src = {logo} style={styles.logo}></Image>
+                              <View style={styles.text}>
+                                <Text style={styles.ouvertura}>OUVERTURA</Text>
+                                <Text style={styles.product}>{props.product}</Text>
+                                <Text style={styles.subtitle} wrap="true">{props.subtitle}</Text>
+                              </View>
+                            </View>
+                          </View>
+                        )
+                      })}
+                    </View>
+                  );
+                }
+                )}
+
+             </View>
+            </Page>
+          </Document>
+        )
+    
+      }
+
+  if(!props.submit){
+    return (
+      <div>
+      </div>
+  )
+  }else if(props.submit){
+    return(
+      <PDFViewer><Labeldoc/></PDFViewer>
+    )
+    
+  }
+}
+
+
+
+
